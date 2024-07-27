fab55f9aa92a326c79383412ca9dd90fe4e3c3cc
Murshid-123 <murshidpgv@gmail.com>
2024-07-27 23:26:56 +0530

    tailwind css setup,cloned constants and assets,lucid raect icons,

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      React app
+    <p className='bg-red-900' >Bismillah</p>
     </>
   )
 }
