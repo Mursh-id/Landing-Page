aeec3718e6146a33461ae5324983e0a15a8186ff
Murshid-123 <murshidpgv@gmail.com>
2024-07-31 00:51:15 +0530

    work flow section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
 import { Navbar } from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection/>
+        <WorkFlow/>
+
+      </div>
     </>
   )
 }
